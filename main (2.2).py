d0d3eeb5574cd587db54bbdb9d8ff21ffafe5cb2
asmku628dfcc2s36622546058 <148202839+asmku628dfcc2s36622546058@users.noreply.github.com>
2023-10-17 14:40:17 +0530

    Add files via upload

---
+++ main (2.2).py
@@ -0,0 +1,20 @@
+# Define the base class player
+class Player:
+    def play(self):
+        print("The player is playing cricker.")
+
+# Define the derived class Batsman
+class Batsman (Player):
+    def play(self):
+        print("The batsman is batting.")
+#define the derived class Bowler
+class Bowler (Player):
+    def play(self):
+        print("The bowler is bowling.")
+#create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+ #call the  playt()  method for each object
+batsman.play()
+bowler.play()
+        
